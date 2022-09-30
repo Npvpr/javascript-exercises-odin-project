abc3f641acb86dd9ac58c28b5f58b55c3d36eb26
Npvpr <nlmnever2@gmail.com>
2022-09-30 18:49:33 +0630

    7 finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let ta = typeof(a), tb = typeof(b);
+    if(ta !== 'number' || tb !== 'number' || a<0 || b<0){
+        console.log(ta, tb)
+        return "ERROR"
+    }
+    if(a>b){
+        let c = a;
+        a = b;
+        b = c;
+    }
+    let sum = 0;
+    for(let i=a;i<=b;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
